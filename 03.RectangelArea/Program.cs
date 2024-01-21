885302c1f467237fb782dde199306d957e82ec33
AKarakochev <133899677+AKarakochev@users.noreply.github.com>
2024-01-21 17:23:15 +0100

    Add files via upload

---
+++ 03.RectangelArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.RectangelArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+
+            int area = sideA * sideB;
+
+            Console.WriteLine(area);
+        }
+    }
+}
